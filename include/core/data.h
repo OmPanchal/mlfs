ba966d531377a21f6a9f74ec708f0993e17c0f76
OmPanchal <oapanchal@gmail.com>
2026-08-16 18:15:37 +0100

    Got One Hot Encoding Working

--- include/core/data.h
+++ include/core/data.h
@@ -11,51 +11,79 @@
 
 namespace mlfs {
 
-/**
- * Loads a purely numeric CSV file into an Eigen matrix
- *
- * @param filepath The file path to the CSV file
- * @param has_header True if the file has a header for the column names
- * @param separator The separator used in the provided CSV file
- * @return A Row Major matrix containing the contents of the file
- */
-RowMatrixXd load_csv_to_row_matrix(const std::string &filepath,
-                                   bool has_header = true, char separator = ',',
-                                   const EncoderMap &encoder_map = {}) {
+class CSVLoader {
+public:
+  CSVLoader(CSVLoaderOptions options = {}) : opts_(std::move(options)) {}
 
-  rapidcsv::LabelParams labelParams =
-      has_header ? rapidcsv::LabelParams(0, -1) : rapidcsv::LabelParams(-1, -1);
-  rapidcsv::Document doc(filepath, labelParams,
-                         rapidcsv::SeparatorParams(separator, true));
-
-  size_t numCols = doc.GetColumnCount();
-  size_t numRows = doc.GetRowCount();
-
-  // Handle empty file
-  if (numRows == 0 || numCols == 0) {
-    return Eigen::MatrixXd(0, 0);
+  /**
+   * Adds an encoder into the encoder map. The encoder map stores a mapping of
+   * column names and their corresponding encoders.
+   * @param col_name The name of the column
+   * @param encoder The Encoder object for that column
+   */
+  void add_encoder(std::string col_name, std::unique_ptr<Encoder> encoder) {
+    encoder_map_[col_name] = std::move(encoder);
   }
-  // Allocate appropriate space to the Eigen Matrix
-  RowMatrixXd matrix(numRows, numCols);
 
-  for (size_t col_idx = 0; col_idx < numCols; col_idx++) {
-    // see if the column name exists in the
-    std::string col_name = doc.GetColumnName(col_idx);
-    auto it = encoder_map.find(col_name);
+  /**
+   * Loads a purely numeric CSV file into an Eigen matrix
+   *
+   * @param filepath The file path to the CSV file
+   * @param has_header True if the file has a header for the column names
+   * @param separator The separator used in the provided CSV file
+   * @return A Row Major matrix containing the contents of the file
+   */
+  RowMatrixXd load_csv_to_row_matrix(const std::string &filepath) {
 
-    if (it != encoder_map.end()) {
-      std::vector<std::string> raw_column = doc.GetColumn<std::string>(col_idx);
-      int colsWritten = it->second->transform(raw_column, matrix, col_idx);
+    rapidcsv::LabelParams labelParams = opts_.has_header
+                                            ? rapidcsv::LabelParams(0, -1)
+                                            : rapidcsv::LabelParams(-1, -1);
+    rapidcsv::Document doc(filepath, labelParams,
+                           rapidcsv::SeparatorParams(opts_.separator, true));
 
-    } else {
-      // ! [NUMERIC ONLY] Parse the column
-      std::vector<double> columnData = doc.GetColumn<double>(col_idx);
-      // Set the respective Eigen Matrix column into the parsed value.
-      matrix.col(col_idx) = Eigen::Map<const Eigen::VectorXd>(
-          columnData.data(), columnData.size());
+    size_t num_cols = doc.GetColumnCount();
+    size_t num_rows = doc.GetRowCount();
+    size_t matrix_cols = num_cols - encoder_map_.size();
+
+    for (auto &[key, value] : encoder_map_) {
+      matrix_cols += value->get_output_column_count();
     }
+
+    // Handle empty file
+    if (num_rows == 0 || num_cols == 0) {
+      return Eigen::MatrixXd(0, 0);
+    }
+    // Allocate appropriate space to the Eigen Matrix
+    RowMatrixXd matrix = RowMatrixXd::Zero(num_rows, matrix_cols);
+
+    size_t matrix_col_idx = 0;
+    for (size_t doc_col_idx = 0; doc_col_idx < num_cols; doc_col_idx++) {
+      // see if the column name exists in the
+      std::string col_name = doc.GetColumnName(doc_col_idx);
+      auto it = encoder_map_.find(col_name);
+
+      if (it != encoder_map_.end()) {
+        std::vector<std::string> raw_column =
+            doc.GetColumn<std::string>(doc_col_idx);
+        int cols_written =
+            it->second->transform(raw_column, matrix, matrix_col_idx);
+
+        matrix_col_idx += cols_written;
+      } else {
+        // Parse Numeric Columns
+        std::vector<double> columnData = doc.GetColumn<double>(doc_col_idx);
+        // Set the respective Eigen Matrix column into the parsed value.
+        matrix.col(matrix_col_idx) = Eigen::Map<const Eigen::VectorXd>(
+            columnData.data(), columnData.size());
+        matrix_col_idx++;
+      }
+    }
+
+    return matrix;
   }
 
-  return matrix;
-}
+private:
+  EncoderMap encoder_map_ = {};
+  const CSVLoaderOptions opts_;
+};
 } // namespace mlfs
--- include/core/encoders.h
+++ include/core/encoders.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "core/types.h"
+#include <iostream>
 #include <unordered_map>
 #include <vector>
 
@@ -8,19 +9,23 @@ namespace mlfs {
 
 class Encoder {
 public:
-  virtual ~Encoder() = default;
-
-  virtual int transform(const std::vector<std::string> &raw_column,
-                        RowMatrixXd &target_matrix, int column_idx) = 0;
-};
-
-class OrdinalEncoder : public Encoder {
   /**
    * The base Encoder Interface
    */
+  virtual ~Encoder() = default;
+
+  [[nodiscard]] virtual int
+  transform(const std::vector<std::string> &raw_column,
+            RowMatrixXd &target_matrix, int column_idx) = 0;
+
+  virtual int get_output_column_count() const { return 1; }
+};
+
+class OrdinalEncoder : public Encoder {
 public:
   /**
    * The Ordinal Encoder class
+   *
    * @param category_map The mapping of the categories to their values
    * @param fallback The default value in case a pair in the mapping do not
    * exist
@@ -41,10 +46,11 @@ public:
    * @param column_idx The index of the column to be encoded
    * @return The number of columns that were modified
    */
-  int transform(const std::vector<std::string> &raw_column,
-                RowMatrixXd &target_matrix, int column_idx) override {
-    for (size_t row = 0; row < raw_column.size(); row++) {
+  [[nodiscard]] int transform(const std::vector<std::string> &raw_column,
+                              RowMatrixXd &target_matrix,
+                              int column_idx) override {
 
+    for (size_t row = 0; row < raw_column.size(); row++) {
       // Check if the current item has a map value
       auto it = category_map_.find(raw_column[row]);
       target_matrix(row, column_idx) =
@@ -57,6 +63,55 @@ public:
 private:
   std::unordered_map<std::string, double> category_map_;
   double fallback_;
+};
+
+class OneHotEncoder : public Encoder {
+public:
+  /**
+   * The One Hot Encoder Class
+   *
+   * @param categories A vector of the different categorires part of the column
+   */
+  OneHotEncoder(const std::vector<std::string> categories) {
+    for (size_t i = 0; i < categories.size(); i++) {
+      categories_.insert({categories[i], i});
+    }
+  };
+
+  /**
+   * Performs one hot encoding on the provided column using `categories`.
+   * Updates the target vector direcly.
+   *
+   * @param raw_column The raw column of strings to be encoded
+   * @param target_matrix The target matrix to store the column
+   * @param column_idx The index of the column to be encoded
+   * @return The number of columns that were modified
+   */
+  [[nodiscard]] int transform(const std::vector<std::string> &raw_column,
+                              RowMatrixXd &target_matrix,
+                              int column_idx) override {
+
+    for (size_t row = 0; row < raw_column.size(); row++) {
+      auto it = categories_.find(raw_column[row]);
+      if (it != categories_.end()) {
+        target_matrix(row, column_idx + it->second) = 1.;
+      } else {
+        target_matrix(row, column_idx + categories_.size()) = 1.;
+      }
+    }
+    return categories_.size() + 1;
+  }
+
+  /**
+   * Returns the number of outputs columns the encoding modifies
+   * @return The number of modified columns (including ones added)
+   */
+  int get_output_column_count() const override {
+    return categories_.size() + 1;
+  }
+
+private:
+  std::unordered_map<std::string, size_t> categories_;
 };
 
 using EncoderMap = std::unordered_map<std::string, std::unique_ptr<Encoder>>;
