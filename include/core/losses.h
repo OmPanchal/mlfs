b67c655a91fb59c56ba9dcd01a3928a548fa40bf
OmPanchal <oapanchal@gmail.com>
2026-08-22 01:15:30 +0100

    Refactored code into header file since methods were made inline

--- include/core/losses.h
+++ include/core/losses.h
@@ -2,6 +2,9 @@
 #include <Eigen/Dense>
 
 namespace mlfs {
+using RowMatrixXd =
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+
 /**
  * Base Loss Interface
  */
@@ -14,7 +17,18 @@ public:
    * @param Y_pred The model's prediction
    * @return The result of the loss function
    */
-  virtual double compute(Eigen::VectorXd &Y, Eigen::VectorXd &Y_pred) const = 0;
+  inline virtual double compute(Eigen::VectorXd &Y,
+                                Eigen::VectorXd &Y_pred) const = 0;
+
+  /**
+   * Returns the gradient of the loss function
+   * @param Y The target value
+   * @param Y_pred The model's prediction
+   * @return The derivative of the loss funciton with respect to the weights
+   */
+  inline virtual RowMatrixXd gradient(Eigen::VectorXd &Y,
+                                      Eigen::VectorXd &Y_pred,
+                                      int batch_size) const = 0;
 };
 
 class MSE : public Loss {
@@ -22,6 +36,15 @@ public:
   MSE() = default;
   ~MSE() = default;
 
-  double compute(Eigen::VectorXd &Y, Eigen::VectorXd &Y_pred) const override;
+  inline double compute(Eigen::VectorXd &Y,
+                        Eigen::VectorXd &Y_pred) const override {
+    return (Y - Y_pred).squaredNorm() / Y.size();
+  };
+
+  inline virtual RowMatrixXd gradient(Eigen::VectorXd &Y,
+                                      Eigen::VectorXd &Y_pred,
+                                      int batch_size) const override {
+    return (-2.0 / batch_size) * (Y - Y_pred);
+  };
 };
 } // namespace mlfs
