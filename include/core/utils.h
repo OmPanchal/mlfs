5c5deb95ad220c3fe3171f1e2c89c1fa9cbd9295
OmPanchal <oapanchal@gmail.com>
2026-08-23 01:48:14 +0100

    Added docs

--- include/core/utils.h
+++ include/core/utils.h
@@ -2,6 +2,12 @@
 #include <random>
 
 namespace mlfs {
+/**
+ * Returns a random value between min and max from a uniform distribution
+ * @param min The minimum value of the sample
+ * @param max The maximum value of the sample
+ * @return A uniformly random number between min and max
+ */
 template <typename T> inline T uniform(double min, double max) {
   std::random_device rd;
   std::mt19937 gen(rd());
