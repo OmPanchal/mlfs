2e4fcde53fbd3dfea4b807aedc3659c3688723b2
OmPanchal <oapanchal@gmail.com>
2026-08-24 19:01:32 +0100

    Made regularisation only affect the weights, not the bias

--- src/regression/linear_regression.cpp
+++ src/regression/linear_regression.cpp
@@ -1,10 +1,8 @@
 #include "regression/linear_regression.h"
+#include "core/errors.h"
 #include "core/utils.h"
 #include <algorithm>
 #include <iostream>
-
-/** TODO: Make the regularisation weight updates only affect the weights rather
- */
 
 namespace mlfs {
 /**
@@ -36,25 +34,22 @@ void LinearRegression::fit_closed_form(const mlfs::RowMatrixXd &X,
 
   // Closed form solution does not exist for l1 regularisation
   if (opts_.alpha != 0) {
-    throw std::runtime_error(
-        "[Error] A closed form solution does not exist "
-        "for the model with alpha > 0 (i.e. a model with l1 regularisation). "
-        "Please try using gradient descent instead.");
+    throw std::runtime_error(LINEAR_REGRESSION_NO_SOLUTION);
   }
 
   // Matrix to be inversed
-  RowMatrixXd Z =
-      ((X.transpose() * X) +
-       (opts_.lambda * X.rows() *
-        Eigen::MatrixXd::Identity(weights_.rows(), weights_.rows())));
+  RowMatrixXd Z = X.transpose() * X;
+  RowMatrixXd R = (opts_.lambda * X.rows() *
+                   Eigen::MatrixXd::Identity(weights_.rows(), weights_.rows()));
+  // Make the final diagonal of the regularisation gradient to zero to make it
+  // not affect the bias
+  R(R.rows() - 1, R.cols() - 1) = 0.;
 
   // Check if the inverse is possible or not
-  if (Z.determinant() == 0) {
-    throw std::runtime_error(
-        "[Error] Closed form solution not possible due to "
-        "non invertable matrix. Please try using gradient descent instead.");
+  if ((Z + R).determinant() == 0) {
+    throw std::runtime_error(MATRIX_NOT_INVERTIBLE);
   } else {
-    weights_ = Z.inverse() * X.transpose() * y;
+    weights_ = (Z + R).inverse() * X.transpose() * y;
   }
 }
 
@@ -64,40 +59,35 @@ void LinearRegression::fit_gd(const mlfs::RowMatrixXd &X,
 
   for (int epoch = 1; epoch < opts_.epochs + 1; epoch++) {
     // Split into batches
-    std::cout << "(EPOCH: " << epoch << ") - ";
     for (int i = 0; i < total_rows; i += opts_.batch_size) {
       // Split the dataset into batches
       int current_batch_size = std::min(opts_.batch_size, total_rows - i);
       RowMatrixXd batch_X = X.middleRows(i, current_batch_size);
       Eigen::VectorXd batch_Y = Y.middleRows(i, current_batch_size);
 
-      // Make prediction and calculate loss with regularisation losses
+      // Make prediction on the batch
       Eigen::VectorXd y = predict(batch_X);
-      double L = opts_.loss->compute(batch_Y, y) +
-                 opts_.lambda * (opts_.alpha * weights_.lpNorm<1>() +
-                                 (1 - opts_.alpha) * weights_.squaredNorm());
 
-      // Output Loss
-      std::cout << "[" << i << " - " << i + current_batch_size
-                << "] - Loss: " << L << " -- ";
-
-      // TODO: Optimise implementation which applies the gradient change to just
-      // the weights Calculate gradients
+      // Calculate loss and regularisation gradients
       Eigen::VectorXd l1_grad =
           (weights_.array().sign() * opts_.alpha).matrix();
 
       Eigen::VectorXd l2_grad =
           (weights_.array() * 2 * (1 - opts_.alpha)).matrix();
 
+      Eigen::VectorXd regularisation_grad = l1_grad + l2_grad;
+
+      // remove affect of gradient to the bias
+      regularisation_grad(regularisation_grad.size() - 1) = 0.;
+
       Eigen::VectorXd dW =
           batch_X.transpose() *
               opts_.loss->gradient(batch_Y, y, batch_Y.rows()) +
-          opts_.lambda * (l1_grad + l2_grad);
+          (opts_.lambda * regularisation_grad);
 
       // Update Weights and biases
       weights_ = weights_ - opts_.learning_rate * dW;
     }
-    std::cout << "\n";
   }
 }
 
