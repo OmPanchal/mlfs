792f406a7c2edf0e54575a7092c2357172bf710d
OmPanchal <oapanchal@gmail.com>
2026-08-27 20:18:18 +0100

    Added new error messages for polynomial regression

--- src/core/errors.cpp
+++ src/core/errors.cpp
@@ -1,7 +1,7 @@
 #include "core/errors.h"
 
 namespace mlfs {
-std::string LINEAR_REGRESSION_NO_SOLUTION =
+std::string REGRESSION_NO_SOLUTION =
     "[Error] A closed form solution does not exist for the model with alpha > "
     "0 (i.e. a model with l1 regularisation). Please try using gradient "
     "descent instead.";
@@ -10,17 +10,25 @@ std::string MATRIX_NOT_INVERTIBLE =
     "[Error] Closed form solution not possible due to "
     "non invertable matrix. Please try using gradient descent instead.";
 
-std::string NEGATIVE_LEARNING_RATE =
-    "[Invalid Argument] learning_rate must be positive";
+std::string NON_POSITIVE_LEARNING_RATE =
+    "[Invalid Argument] learning_rate must be positive.";
 
-std::string NEGATIVE_EPOCHS = "[Invalid Argument] epochs must be positive";
+std::string NON_POSITIVE_EPOCHS = "[Invalid Argument] epochs must be positive.";
 
-std::string NEGATIVE_BATCH_SIZE =
-    "[Invalid Argument] batch_size must be positive";
+std::string NON_POSITIVE_BATCH_SIZE =
+    "[Invalid Argument] batch_size must be positive.";
 
-std::string LINEAR_REGRESSION_NEGATIVE_LAMBDA =
-    "[Invalid Argument] lambda must be positive";
+std::string REGRESSION_NEGATIVE_LAMBDA =
+    "[Invalid Argument] lambda must be positive.";
 
-std::string LINEAR_REGRESSION_OUT_OF_RANGE_ALPHA =
-    "[Invalid Argument] alpha must be in the interval [0, 1]";
+std::string REGRESSION_OUT_OF_RANGE_ALPHA =
+    "[Invalid Argument] alpha must be in the interval [0, 1].";
+
+std::string POLYNOMIAL_REGRESSION_LINEAR_ORDER =
+    "[Invalid Argument] order must be greater than two for this polynomial "
+    "regression model. Make use of Linear Regression if you wish for a "
+    "polynomial regression model with order=1.";
+
+std::string POLYNOMIAL_REGRESSION_NON_POSITIVE_ORDER =
+    "[Invalid Argument] order must be positive.";
 } // namespace mlfs
