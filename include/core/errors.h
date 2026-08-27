7dd2ddecdbce8933d964c9691c0b529f2e984761
OmPanchal <oapanchal@gmail.com>
2026-08-27 20:17:41 +0100

    Added polynomial regression error messages

--- include/core/errors.h
+++ include/core/errors.h
@@ -4,17 +4,21 @@
 
 namespace mlfs {
 // Error Messages
-extern std::string LINEAR_REGRESSION_NO_SOLUTION;
+extern std::string REGRESSION_NO_SOLUTION;
 
 extern std::string MATRIX_NOT_INVERTIBLE;
 
-extern std::string NEGATIVE_LEARNING_RATE;
+extern std::string NON_POSITIVE_LEARNING_RATE;
 
-extern std::string NEGATIVE_EPOCHS;
+extern std::string NON_POSITIVE_EPOCHS;
 
-extern std::string NEGATIVE_BATCH_SIZE;
+extern std::string NON_POSITIVE_BATCH_SIZE;
 
-extern std::string LINEAR_REGRESSION_NEGATIVE_LAMBDA;
+extern std::string REGRESSION_NEGATIVE_LAMBDA;
 
-extern std::string LINEAR_REGRESSION_OUT_OF_RANGE_ALPHA;
+extern std::string REGRESSION_OUT_OF_RANGE_ALPHA;
+
+extern std::string POLYNOMIAL_REGRESSION_LINEAR_ORDER;
+
+extern std::string POLYNOMIAL_REGRESSION_NON_POSITIVE_ORDER;
 } // namespace mlfs
