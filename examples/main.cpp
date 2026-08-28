e497732fcca653563e16f6bb9b2953d3dd90687a
OmPanchal <oapanchal@gmail.com>
2026-08-28 19:56:50 +0100

    Partially tested to make sure that the dataset changes to the model are working well

--- examples/main.cpp
+++ examples/main.cpp
@@ -10,9 +10,18 @@
 int main() {
   mlfs::CSVLoader loader = mlfs::CSVLoader();
   mlfs::CSVDataset data = loader.load_csv_to_row_matrix(
-      "/home/om/Programming/C++Sandbox/mlfs/data/numeric_data.csv", "col1");
+      "/home/om/Programming/C++Sandbox/mlfs/data/numeric_data_2.csv", "col2");
 
-  std::cout << data.get_features() << "\n\n";
-  std::cout << data.get_raw_features() << "\n\n";
-  std::cout << data.get_target() << "\n\n";
+  mlfs::LinearRegression model =
+      mlfs::LinearRegression(2, {.learning_rate = 0.01,
+                                 .epochs = 1000,
+                                 .batch_size = 1,
+                                 .solver = mlfs::SolverType::GD,
+                                 .lambda = 0,
+                                 .alpha = 0.5});
+
+  // fit the model on the dataset
+  model.fit(data);
+
+  std::cout << model.get_weights();
 }
